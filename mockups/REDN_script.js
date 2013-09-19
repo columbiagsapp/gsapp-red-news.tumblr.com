e0ad509f4f1efe48e806802ae0958c3f636a464d
ebberly <estrathairn@gmail.com>
2013-09-19 11:37:36 -0400

    working scroll stop for header and nav

--- mockups/REDN_script.js
+++ mockups/REDN_script.js
@@ -1,6 +1,29 @@
 $(document).ready( function() {
 console.log("REDN Loaded");
 
+$(window).scroll(function(){
+				var scrollTop = 130;
+				var scrolled = $(window).scrollTop();
+				if(scrolled >= scrollTop){	
+					$('#header').css({position: 'fixed', top: '-150px'})
+					$('#nav').css({position: 'fixed', top: '57px'})
+
+
+					}
+				if(scrolled < scrollTop){	
+					$('#header').css({position: 'relative', top: '0px'})
+					$('#nav').css({position: 'relative', top: '0px'})
+
+				}
+				
+				//move exumas
+				/*var exumasTop = 100;
+				if (scrolled >= exumasTop){	$('.header h2 img').css({ position: 'fixed', top : '20px',}) }
+				if (scrolled < exumasTop){	$('.header h2 img').removeAttr('style'); }*/
+				});
+
+
+
 $('.realpost').click(function(){
 	console.log('Realpost clicked.');
 
