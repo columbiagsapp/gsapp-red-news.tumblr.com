da048532214c5366cdfdc5cb8e6bcbe6ecdf219a
ebberly <estrathairn@gmail.com>
2013-09-19 12:57:56 -0400

    scroll stop updates

--- mockups/REDN_script.js
+++ mockups/REDN_script.js
@@ -2,17 +2,22 @@ $(document).ready( function() {
 console.log("REDN Loaded");
 
 $(window).scroll(function(){
-				var scrollTop = 130;
+				var scrollTop = 180;
 				var scrolled = $(window).scrollTop();
+					console.log(scrolled);
+
 				if(scrolled >= scrollTop){	
 					$('#header').css({position: 'fixed', top: '-150px'})
-					$('#nav').css({position: 'fixed', top: '57px'})
+					$('#nav').css({position: 'fixed', top: '114px'})
+					$('#blogroll').css({position: 'relative', 'margin-top': '260px'})
 
 
 					}
 				if(scrolled < scrollTop){	
 					$('#header').css({position: 'relative', top: '0px'})
 					$('#nav').css({position: 'relative', top: '0px'})
+					$('#blogroll').css({position: 'relative', 'margin-top': '0px'})
+
 
 				}
 				
