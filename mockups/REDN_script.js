8df10773d4fab28ce501a3985d771d9fa84ff9ef
ebberly <estrathairn@gmail.com>
2013-09-21 19:52:27 -0400

    updates

--- mockups/REDN_script.js
+++ mockups/REDN_script.js
@@ -5,11 +5,12 @@ $(window).scroll(function(){
 				var scrollTop = 150;
 
 				var scrolled = $(window).scrollTop();
-
+				console.log(scrolled);
 				if(scrolled >= scrollTop){	
 					$('#logo').css({position: 'fixed', top: '-120px'})
 					$('#nav').css({position: 'fixed', top: '144px'})
 					//$('#blogroll').css({position: 'relative', 'margin-top': '260px'})
+					console.log('fixed');
 
 
 					}
@@ -19,11 +20,9 @@ $(window).scroll(function(){
 					$('#blogroll').css({position: 'relative', 'margin-top': '0px'})
 
 
-
 				}
 
 				var opacityfactor = scrolled/120;
-				console.log(opacityfactor);
 				$("#headerfademask").css({'opacity': opacityfactor});
 
 				});
